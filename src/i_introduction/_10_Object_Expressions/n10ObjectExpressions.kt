9a34113f7b0e19f59c34f29d35773a68c796d413
Maxim Lazeba <m.lazeba@gmail.com>
2017-10-14 23:43:46 +0300

    Task 10, Task 11

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -18,6 +18,10 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, { a: Int, b: Int -> b - a})
+    Collections.sort(arrayList, object : Comparator<Int> {
+        override fun compare(o1: Int, o2: Int): Int {
+            return o2 - o1;
+        }
+    })
     return arrayList
 }
